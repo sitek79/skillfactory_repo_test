84f9e5814d19c1903be2bcbdb3e52f04c5118761
sitek79 <atikhonov79@gmail.com>
2021-09-23 10:40:04 +0300

    Initial Commit. Package Recursion Add

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import recursion.Recursion;
+
+public class App {
+    public static void main(String[] args) {
+        Recursion rec = new Recursion();
+        rec.recursion();
+    }
+}
---
+++ src/recursion/Recursion.java
@@ -0,0 +1,10 @@
+package recursion;
+
+public class Recursion {
+    public void recursion() {
+        Factorial f = new Factorial();
+        System.out.println("Фaктopиaл 3 равен " + f.fact(3));
+        System.out.println("Фaктopиaл 4 равен " + f.fact(4));
+        System.out.println("Фaктopиaл 5 равен " + f.fact(5));
+    }
+}
