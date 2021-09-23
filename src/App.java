7247bfd186fdf60d57417c91ba8e95bbfbafa754
sitek79 <atikhonov79@gmail.com>
2021-09-23 13:38:08 +0300

    Feature Clock. Add.

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
+import clock.Clock;
 import recursion.Recursion;
 
 public class App {
     public static void main(String[] args) {
         Recursion rec = new Recursion();
         rec.recursion();
+        Clock clk = new Clock();
+        clk.printClock();
     }
 }
---
+++ src/clock/Clock.java
@@ -0,0 +1,17 @@
+package clock;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Clock {
+    public void printClock() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy");
+        Calendar calendar = new GregorianCalendar(2017, Calendar.JANUARY , 25);
+        calendar.set(Calendar.HOUR, 10);
+        calendar.set(Calendar.MINUTE, 42);
+        calendar.set(Calendar.SECOND, 12);
+        calendar.roll(Calendar.MONTH, -2);
+        System.out.println(dateFormat.format(calendar.getTime()));
+    }
+}
